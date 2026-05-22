73cf972d5e3d67cfc5bb86c7d4d9a214a65fcb5a
Danilo Borges Leão <61187963+DaniloBorges44@users.noreply.github.com>
2026-05-22 13:36:00 -0300

    Mudanças de layout e função de executar emulador
    Adicionado papel de parede, efeito de hover nos botões e alterada função para executar comando do botão principal

--- main.py
+++ main.py
@@ -11,8 +11,6 @@ def update_carousel(index): # Inicia e tualiza o carrossel
     main_button.configure(
         text=item["text"],
         image=item["image"],
-        command=item["command"],
-        hover_color=item["hover_color"],
         fg_color=item["fg_color"]
     )
     
@@ -28,20 +26,27 @@ def prev_item():  # Passa para o item anterior da lista
     index = (index - 1) % len(emulators)
     update_carousel(index)
 
-def execute_playstation():
-    subprocess.run(["notepad.exe"])
-    
-def execute_gba():
-    subprocess.run(["notepad.exe"])
-
-def execute_nintendo_64():
-    subprocess.run(["notepad.exe"])
+def execute_emulator(): # Executa o emulador selecionado
+     match index:
+        case 0:
+            print("Executando PCSX2")
+            #subprocess.run(["notepad.exe"])
+        case 1:
+            print("Executando mGBA")
+        case 2:
+            print("Executando Simple64")
+        case _:
+            print("Emulador não encontrado")
+            
+def button_effect(button, border_color, border_width): # Adiciona o efeito de borda ao botão em hover
+    button.configure(border_color=border_color, border_width=border_width)
 # --- Functions ---
 
 # --- Images ---
-psx2_image = CTK.CTkImage(light_image=Image.open("imgs\PCSX2_logo.png"), dark_image=Image.open("imgs\PCSX2_logo.png"), size=(100, 100))
-mgba_image = CTK.CTkImage(light_image=Image.open("imgs\mGBA_logo.png"), dark_image=Image.open("imgs\mGBA_logo.png"), size=(100, 100))
-simple64_image = CTK.CTkImage(light_image=Image.open("imgs\simple64_logo.png"), dark_image=Image.open("imgs\simple64_logo.png"), size=(100, 100))
+psx2_image = CTK.CTkImage(light_image=Image.open(r"imgs\PCSX2_logo.png"), dark_image=Image.open(r"imgs\PCSX2_logo.png"), size=(100, 100))
+mgba_image = CTK.CTkImage(light_image=Image.open(r"imgs\mGBA_logo.png"), dark_image=Image.open(r"imgs\mGBA_logo.png"), size=(100, 100))
+simple64_image = CTK.CTkImage(light_image=Image.open(r"imgs\simple64_logo.png"), dark_image=Image.open(r"imgs\simple64_logo.png"), size=(100, 100))
+bg_image = CTK.CTkImage(light_image=Image.open(r"imgs\bg_img.jpg"), dark_image=Image.open(r"imgs\bg_img.jpg"), size=(1366, 768))
 # --- Images ---
 
 
@@ -50,23 +55,23 @@ emulators = [
     {
         "text": "PlayStation 2",
         "image": psx2_image,
-        "command": execute_playstation,
-        "hover_color":"#4860b1",
-        "fg_color":"#1d3d6c"
+        "fg_color":"#1d3d6c",
+        "border_color":"#4860b1",
+        "border_width": 4
     },
     {
         "text": "Game Boy Adventure",
         "image": mgba_image,
-        "command": execute_gba,
-        "hover_color":"#857de1",
-        "fg_color":"#5b33b0"                                                                                  
+        "fg_color":"#5b33b0",
+        "border_color":"#857de1",
+        "border_width": 4                                                                               
     },
     {
         "text": "Nintendo 64",
         "image": simple64_image,
-        "command": execute_nintendo_64,
-        "hover_color":"#e94527",
-        "fg_color":"#9e0b0f"  
+        "fg_color":"#9e0b0f",
+        "border_color":"#e94527",
+        "border_width": 4
     }
 ]
 # --- Emulators ---
@@ -93,25 +98,36 @@ print(f"Controller: {joystick.get_name()} connected.")
 # --- Interface ---
 CTK.set_appearance_mode("dark") # Define o modo de aparência para "dark"
 app.attributes('-fullscreen', True) # Define o aplicativo para tela cheia
-CTK.CTkFrame(app, width=1050, height=600, fg_color="#0F0F0F").place(relx=0.5, rely=0.5,  anchor=CTK.CENTER) # Cria o fundo do carrossel
+CTK.CTkLabel(app, image=bg_image, text="").place(relwidth=1, relheight=1) # Define a imagem de fundo
+CTK.CTkFrame(app, width=1050, height=600, fg_color="#0F0F0F", border_color="#252525", border_width=2).place(relx=0.5, rely=0.5,  anchor=CTK.CENTER) # Cria o fundo do carrossel
 
 main_button = CTK.CTkButton( # Botão principal do carrossel
     app,
     text="",
     image=None,
     compound="top",
-    hover_color="white",
+    hover=False,
     fg_color="white",
     bg_color="#0F0F0F",
     width=900,
-    height=500
+    height=500,
+    command=execute_emulator
 )
 
 main_button.place(relx=0.5, rely=0.5, anchor=CTK.CENTER) # Posiciona o botão principal
+main_button.bind("<Enter>", lambda e: button_effect(main_button, emulators[index]["border_color"], emulators[index]["border_width"])) # Adiciona o efeito de hover ao botão principal
+main_button.bind("<Leave>", lambda e: button_effect(main_button, "black", 0)) # Remove o efeito de hover quando o mouse sair do botão principal
 
-CTK.CTkButton(app, text=">", width=40, height=40, corner_radius=80, bg_color="#0F0F0F", command=next_item).place(relx=0.844, rely=0.5) # Botão de avançar
 
-CTK.CTkButton(app, text="<", width=40, height=40, corner_radius=80, bg_color="#0F0F0F", command=prev_item).place(relx=0.12, rely=0.5) # Botão de retroceder
+left_button = CTK.CTkButton(app, text=">", width=40, height=40, corner_radius=80, fg_color ="#005fbe", hover_color= "#0075eb", bg_color="#0F0F0F", command=next_item) # Botão de avançar
+left_button.place(relx=0.844, rely=0.5) 
+right_button = CTK.CTkButton(app, text="<", width=40, height=40, corner_radius=80, fg_color ="#005fbe", hover_color= "#0075eb", bg_color="#0F0F0F", command=prev_item) # Botão de retroceder
+right_button.place(relx=0.12, rely=0.5)     
+
+left_button.bind("<Enter>", lambda e: button_effect(left_button, "#4aa5ff", 2)) 
+left_button.bind("<Leave>", lambda e: button_effect(left_button, "black", 0))
+right_button.bind("<Enter>", lambda e: button_effect(right_button, "#4aa5ff", 2))
+right_button.bind("<Leave>", lambda e: button_effect(right_button, "black", 0))
 
 exit_button = CTK.CTkButton( # Botão de sair
     app, 
@@ -119,11 +135,15 @@ exit_button = CTK.CTkButton( # Botão de sair
     font=(None, 18),  
     width=40, 
     height=30,
-    border_width=3,
+    border_width=2,
     border_color="black",
-    fg_color="#FF0000",
-    hover_color="#D81313",
-    command=app.destroy).place(x=app.winfo_screenwidth() - 80, y=20)
+    fg_color="#DB0C0C",
+    hover_color="#FF0000",
+    command=app.destroy)
+
+exit_button.place(x=app.winfo_screenwidth() - 80, y=20)
+exit_button.bind("<Enter>", lambda e: button_effect(exit_button, "#FF0000", 4))
+exit_button.bind("<Leave>", lambda e: button_effect(exit_button, "black", 2))  
 # --- Interface ---
 
 # Inicializa a interface
